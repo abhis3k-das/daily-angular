f810799f2f860406126e6a5a8211cb6af41cc0a9
abhis3k-das <abhis3k.das@gmail.com>
2026-08-30 18:14:43 +0530

    pacticed ng-content

--- 08__Content_Projection_Reusable_Panel_Shell/src/app/app.ts
+++ 08__Content_Projection_Reusable_Panel_Shell/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Panel } from './components/panel/panel';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , Panel],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('08__Content_Projection_Reusable_Panel_Shell');
 }
